dbc5e27a9f470960b4a9d6f7eee23f779bf1382d
whosmeya <whosmeya@gmail.com>
2019-07-06 23:39:06 +0800

    update

---
+++ src/components/Tabbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <van-tabbar v-model="active">
+    <van-tabbar-item name="home" icon="home-o">首页</van-tabbar-item>
+    <van-tabbar-item name="about" icon="notes-o">关于</van-tabbar-item>
+    <van-tabbar-item name="me" icon="contact">我的</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<script>
+import { Tabbar, TabbarItem } from "vant";
+
+export default {
+  name: "tabbar",
+  components: {
+    [Tabbar.name]: Tabbar,
+    [TabbarItem.name]: TabbarItem
+  },
+  data() {
+    return {
+      active: "home"
+    };
+  },
+  methods: {
+    beforeCreate() {
+      console.log(this, "beforeCreate");
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Tabbar />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import Tabbar from "@/components/Tabbar.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Tabbar
+  },
+  data() {
+    return {
+      active: "home"
+    };
   }
-}
+};
 </script>
